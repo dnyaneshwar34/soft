5db0dddc87568819745f29efc8074d4aab285446
dnyaneshwar35 <dnyaneshwar35kinikardnyaneshwar29@gmail.com>
2023-07-31 12:35:42 +0530

    added code

--- src/test/java/call/Myntra.java
+++ src/test/java/call/Myntra.java
@@ -1,28 +1,22 @@
 package call;
 
-import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Newcall {
-	
+public class Myntra {
 	WebDriver driver;
-	
-	
   @Test
-  public void verifycall() {
+  public void verifymyngtra() {
 	  
 	  WebDriverManager.chromedriver().setup();
 	  driver = new ChromeDriver();
-	  driver.get("https://www.ajio.com/");
-	  
+	  driver.get("https://www.myntra.com/");
 	  driver.manage().deleteAllCookies();
 	  driver.quit();
 	  
+	  
   }
-  
-  
-  
 }
